983be088ceef21ce3aa57460d2742df8b838bae1
lionad <lionad@takecloud.cn>
2018-12-04 15:19:12 +0800

    ADD 百度/微信/支付宝 app.json 相互转换;

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const path = require('path')
 
 import Compiler from '../lib/index'
+import ConfigHandle from '../lib/ConfigHandle/index'
 
-var unsolvedFolder = path.resolve(__dirname, '../src/swan/swan-template')
-// console.log(unsolvedFolder)
+/** 使用 swan 模板Page文件测试 compiler */
 
-Compiler.convert(unsolvedFolder, {
-    plat: 'swan',
-    type: ['template', 'style'],
+let unsolvedFolder = path.resolve(__dirname, '../src/swan/swan-template')
+
+// Compiler.convert(unsolvedFolder, {
+//     plat: 'swan',
+//     type: ['template', 'style'],
+// })
+
+/** 测试小程序appJSON互相转换 */
+
+let wechatConfigFile = path.resolve(__dirname, '../src/wechat/app.json')
+
+ConfigHandle.convert(wechatConfigFile, {
+    from: 'wechat',
+    to: 'ali'
 })
